4ce79948967b92e5a837572f85246974ac2ab03d
bohovaddinrozmetov24-lang <bohovaddinrozmetov24@gmail.com>
2025-08-07 12:34:59 +0500

    init

---
+++ index.js
@@ -0,0 +1,86 @@
+// let n = 15;
+// for (let i = 1; i < n; i += 2) {
+//     console.log(i);
+// 
+
+// 2-masala
+
+// let n = 10;
+// let sum = 0;
+// for (let i = 2; i <= n; i += 2) {
+//     sum += i;
+// }
+// console.log("Juft sonlar yig'indisi:", sum);
+
+// 3-masala
+
+// let num = 12345;
+// let reversed = 0;
+
+// for (; num > 0; num = Math.floor(num / 10)) {
+//     let digit = num % 10;
+//     reversed = reversed * 10 + digit;
+// }
+// console.log("Teskari raqam:", reversed);
+
+// 4-masala
+
+// let n = 9;
+// let product = 1;
+
+// for (let i = 1; i <= n; i += 2) {
+//     product *= i;
+// }
+// console.log("Toq sonlar ko'paytmasi:", product);
+
+// 5-masala
+
+// for (let i = 2; i <= 50; i += 2) {
+//     console.log(i);
+// }
+
+// 6-masala
+
+// let str = "Hello World";
+// let count = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i] !== " ") {
+//         count++;
+//     }
+// }
+// console.log("Harf soni:", count);
+
+// 7-masala
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// 8-masala
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// 9-masala
+
+// let num = 123456;
+// let count = 0;
+
+// for (; num > 0; num = Math.floor(num / 10)) {
+//     count++;
+// }
+// console.log("Son uzunligi:", count);
+
+// 10-masala
+
+// let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+// for (let i = 0; i < alphabet.length; i++) {
+//     console.log((i + 1) + ": " + alphabet.charAt(i));
+// }
